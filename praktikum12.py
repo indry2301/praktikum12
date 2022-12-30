a92dad7040f336cd0a093bc8b6a9d37934182a14
indry widiyani <indrywidiyani5@gmail.com>
2022-12-30 12:16:46 +0700

    menambahkan praktikum12.py

---
+++ praktikum12.py
@@ -0,0 +1,27 @@
+print("Nama     : Indry Widiyani")
+print("NIM      : 312210060")
+print("Kelas    : TI.22.C1")
+
+txt = 'Hello World'
+print()
+
+print("Menghitung jumlah karakter: ")
+print(len(txt))
+print("Mengambil karakter terakhir: ")
+print(txt[-1])
+
+print("Mengambil karakter index ke-2 sampai index ke-4 (llo): ")
+print(txt[2:5])
+
+print("Menghilangkan spasi pada text tersebut (HelloWorld): ")
+print(txt.replace(" "," "))
+
+print("Mengubah text menjadi huruf besar: ")
+print(txt.upper())
+
+print("Mengubah text menjadi huru kecil: ")
+print(txt.lower())
+
+print("Mengganti karakter H menjadi karakter J: ")
+print(txt.replace("H", "J"))
+print()
